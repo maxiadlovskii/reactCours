6dbe40c6c97c91605a5e6749ac0ebee9ae973e56
maxiadlovskii <max.iadlovskii@gmail.com>
2017-11-25 13:36:02 +0200

    Commit 2

--- src/Article.js
+++ src/Article.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react'
 import CommentList from './CommentList';
-
 export default class Article extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             isOpen: true
         }
