5b66ff8c258bfad0eec2677ecf72f40c1cbe30b6
maxiadlovskii <max.iadlovskii@gmail.com>
2018-02-17 19:55:38 +0200

    Add immutable lib

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,16 +1,28 @@
 import {normalizedArticles as defaultArticles} from '../fixtures'
 import {arrToMap, mapToArr} from '../helpers'
 import {DELETE_ARTICLE, ADD_COMMENT, LOAD_ARTICLES} from '../constans'
-
-export default (articleState = {}, action)=>{
+import {Map, Record} from 'immutable'
+//Опис вигляду статті, типу модель
+const ArticleRecord = Record({
+    text: undefined,
+    title: '',
+    id: undefined,
+    comments: []
+})
+const defaultState = new Map({})
+export default (articleState = defaultState, action)=>{
     const {type, payload, response, randomId} = action
 
     switch(type){
         case DELETE_ARTICLE:
+            return articleState.delete(payload.id);
+            /*
             const tmpState = {...articleState}
             delete tmpState[payload.id]
             return tmpState
+            */
         case ADD_COMMENT:
+            /*
             const article = articleState[payload.articleId]
             return{
                 ...articleState,
@@ -19,8 +31,11 @@ export default (articleState = {}, action)=>{
                     comments: (article.comments || []).concat(randomId)
                 }
             }
+            */
+            return articleState.updateIn([payload.articleId, 'comments'], comments=> comments.concat({randomId})) //<-- Оновлюємо іммутабельні данні
+                                                                                                                    // updateIn( [в чому оновлювати, що оновлювати], як оновлювати )
         case LOAD_ARTICLES:
-            return arrToMap(response)
+            return arrToMap(response, ArticleRecord) //<-- Передаєм модель статті
     }
 
     return articleState
