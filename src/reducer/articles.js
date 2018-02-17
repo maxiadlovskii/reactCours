4136f9317257aed563e05834c2b02c20a76e3fc9
maxiadlovskii <max.iadlovskii@gmail.com>
2018-02-17 20:51:27 +0200

    Add articles loading

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,7 +1,7 @@
 import {normalizedArticles as defaultArticles} from '../fixtures'
 import {arrToMap, mapToArr} from '../helpers'
-import {DELETE_ARTICLE, ADD_COMMENT, LOAD_ARTICLES} from '../constans'
-import {Map, Record} from 'immutable'
+import {DELETE_ARTICLE, ADD_COMMENT, LOAD_ARTICLES, START, SUCCESS, FAIL} from '../constans'
+import {OrderedMap, Map, Record} from 'immutable'
 //Опис вигляду статті, типу модель
 const ArticleRecord = Record({
     text: undefined,
@@ -9,13 +9,19 @@ const ArticleRecord = Record({
     id: undefined,
     comments: []
 })
-const defaultState = new Map({})
+const ReducerState = Record({
+    loading: false,
+    loaded: false,
+    entities: new OrderedMap({})
+})
+
+const defaultState = new ReducerState();
 export default (articleState = defaultState, action)=>{
     const {type, payload, response, randomId} = action
 
     switch(type){
         case DELETE_ARTICLE:
-            return articleState.delete(payload.id);
+            return articleState.deleteIn(['entities', payload.id]);
             /*
             const tmpState = {...articleState}
             delete tmpState[payload.id]
@@ -32,10 +38,16 @@ export default (articleState = defaultState, action)=>{
                 }
             }
             */
-            return articleState.updateIn([payload.articleId, 'comments'], comments=> comments.concat({randomId})) //<-- Оновлюємо іммутабельні данні
-                                                                                                                    // updateIn( [в чому оновлювати, що оновлювати], як оновлювати )
-        case LOAD_ARTICLES:
-            return arrToMap(response, ArticleRecord) //<-- Передаєм модель статті
+            return articleState.updateIn(['entities', payload.articleId, 'comments'], comments=> comments.concat({randomId})) //<-- Оновлюємо іммутабельні данні
+                                                                                                         // updateIn( [в чому оновлювати, що оновлювати], як оновлювати )
+
+        case LOAD_ARTICLES+START:
+            return articleState.set('loading', true);
+        case LOAD_ARTICLES+SUCCESS:
+            return articleState
+                .set( 'entities', arrToMap(response, ArticleRecord),  ) //<-- Передаєм модель статті
+                .set('loading', false)
+                .set('loaded', true)
     }
 
     return articleState
